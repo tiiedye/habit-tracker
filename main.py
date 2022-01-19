c10cf647c8611a94d8a87e990010def382149f89
tiiedye <tiiedye@gmail.com>
2022-01-19 11:36:55 -0800

    adding the creation of a graph

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import requests
 from dotenv import load_dotenv
 import os
 
-# To run this program you will need to create your own .env file and use your own credentials
-# pixela user documentation: https://docs.pixe.la/entry/post-user
+# NOTE: To run this program you will need to create your own .env file and use your own credentials
+# NOTE: pixela user documentation: https://docs.pixe.la/entry/post-user
 load_dotenv()
 user_token = os.getenv('PIXELA_TOKEN')
 username = os.getenv('PIXELA_USERNAME')
@@ -17,5 +17,23 @@ user_params = {
     "notMinor": "yes"
 }
 
-response = requests.post(url=pixela_endpoint, json=user_params)
-print(response.text)
+# NOTE: Comment this in when creating a user!
+# response = requests.post(url=pixela_endpoint, json=user_params)
+
+graph_endpoint = f"{pixela_endpoint}/{username}/graphs"
+
+# Developer Note: If using or testing this app, you can change these configs as needed to suit your needs
+graph_config = {
+    "id": "graph1",
+    "name": "Meditation Graph",
+    "unit": "minute",
+    "type": "float",
+    "color": "ajisai"
+}
+
+headers = {
+    "X-USER-TOKEN": user_token
+}
+
+graph_response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+
